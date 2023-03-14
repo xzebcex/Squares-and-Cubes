de0f6fe4529e8c122f956889a9ab0fa56e2cfbd9
Aman Pandit <amanpandit3027@gmail.com>
2023-03-14 08:13:01 +0530

    Create squares.py
    This program print the square of first 10 number through 1-10.

---
+++ squares.py
@@ -0,0 +1,8 @@
+# Squares
+# This program print the square of first 10 number through 1-10.
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+print(squares)
